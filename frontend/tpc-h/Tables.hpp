5bab9857b19e4e90414439e06ab7a3a34becb6f2
alicia-lyu <wlyu27@wisc.edu>
2025-03-05 23:25:55 +0000

    Tables with template

--- frontend/tpc-h/Tables.hpp
+++ frontend/tpc-h/Tables.hpp
@@ -2,18 +2,46 @@
 #include <ostream>
 #include "../shared/Types.hpp"
 
-struct part_t {
+template <typename K, auto K::* ...Members>
+class KeyPrototype {
+    friend std::ostream& operator<<(std::ostream& os, const K& record)
+    {
+        ((os << Members << ": " << record.*Members << ", "), ...);
+        return os;
+    }
+};
+
+template <typename T, auto T::* ...Members>
+class RecordPrototype {
+    static constexpr unsigned rowSize() { return (sizeof(std::declval<T>().*Members) + ...); }
+    
+    template <class K, auto K::* ...KMembers>
+    static unsigned foldKey(uint8_t* out, const K& key)
+    {
+        unsigned pos = 0;
+        ((pos += fold(out + pos, key.*KMembers)), ...);
+        return pos;
+    }
+
+    template <class K, auto K::* ...KMembers>
+    static unsigned unfoldKey(const uint8_t* in, K& key)
+    {
+        unsigned pos = 0;
+        ((pos += unfold(in + pos, key.*KMembers)), ...);
+        return pos;
+    }
+
+    static constexpr unsigned maxFoldLength() { return (sizeof(std::declval<T>().*Members) + ...); }
+};
+
+struct part_base {
     static constexpr int id = 0;
-    struct Key {
+    struct key_base {
         static constexpr int id = 0;
         Integer p_partkey;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "partkey: " << record.p_partkey;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::p_partkey> {};
 
     Varchar<55> p_name;
     Varchar<25> p_mfgr;
@@ -25,18 +53,25 @@ struct part_t {
     Varchar<23> p_comment;
 };
 
-struct supplier_t {
-    static constexpr int id = 1;
-    struct Key {
-        static constexpr int id = 1;
-        Integer s_suppkey
+struct part_t : public part_base, public RecordPrototype<part_base,
+    &part_base::p_name,
+    &part_base::p_mfgr,
+    &part_base::p_brand,
+    &part_base::p_type,
+    &part_base::p_size,
+    &part_base::p_container,
+    &part_base::p_retailprice,
+    &part_base::p_comment>
+{};
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "s_id: " << record.s_suppkey;
-            return os;
-        }
-    }
+struct supplier_base {
+    static constexpr int id = 1;
+    struct key_base {
+        static constexpr int id = 1;
+        Integer s_suppkey;
+    };
+
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::s_suppkey> {};
 
     Varchar<25> s_name;
     Varchar<40> s_address;
@@ -44,39 +79,35 @@ struct supplier_t {
     Varchar<15> s_phone;
     Numeric s_acctbal;
     ;Varchar<101> s_comment;
-}
+};
 
-struct partsupp_t {
+struct supplier_t : public supplier_base, public RecordPrototype<supplier_base, &supplier_base::s_name, &supplier_base::s_address, &supplier_base::s_nationkey, &supplier_base::s_phone, &supplier_base::s_acctbal, &supplier_base::s_comment> {};
+
+struct partsupp_base {
     static constexpr int id = 2;
-    struct Key {
+    struct key_base {
         static constexpr int id = 2;
         Integer ps_partkey;
         Integer ps_suppkey;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "ps_partkey: " << record.ps_partkey << ", ps_suppkey: " << record.ps_suppkey;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::ps_partkey, &key_base::ps_suppkey> {};
 
     Integer ps_availqty;
     Numeric ps_supplycost;
     Varchar<199> ps_comment;
 };
 
-struct customer_t {
+struct partsupp_t : public partsupp_base, public RecordPrototype<partsupp_base, &partsupp_base::ps_availqty, &partsupp_base::ps_supplycost, &partsupp_base::ps_comment> {};
+
+struct customer_base {
     static constexpr int id = 3;
-    struct Key {
+    struct key_base {
         static constexpr int id = 3;
         Integer c_custkey;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "c_custkey: " << record.c_custkey;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::c_custkey> {};
 
     Varchar<25> c_name;
     Varchar<40> c_address;
@@ -87,18 +118,16 @@ struct customer_t {
     Varchar<117> c_comment;
 };
 
-struct orders_t {
+struct customer_t : public customer_base, public RecordPrototype<customer_base, &customer_base::c_name, &customer_base::c_address, &customer_base::c_nationkey, &customer_base::c_phone, &customer_base::c_acctbal, &customer_base::c_mktsegment, &customer_base::c_comment> {};
+
+struct orders_base {
     static constexpr int id = 4;
-    struct Key {
+    struct key_base {
         static constexpr int id = 4;
         Integer o_orderkey;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "o_orderkey: " << record.o_orderkey;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::o_orderkey> {};
 
     Integer o_custkey;
     Varchar<1> o_orderstatus;
@@ -110,19 +139,17 @@ struct orders_t {
     Varchar<79> o_comment;
 };
 
-struct lineitem_t {
+struct orders_t : public orders_base, public RecordPrototype<orders_base, &orders_base::o_custkey, &orders_base::o_orderstatus, &orders_base::o_totalprice, &orders_base::o_orderdate, &orders_base::o_orderpriority, &orders_base::o_clerk, &orders_base::o_shippriority, &orders_base::o_comment> {};
+
+struct lineitem_base {
     static constexpr int id = 5;
-    struct Key {
+    struct key_base {
         static constexpr int id = 5;
         Integer l_orderkey;
         Integer l_linenumber;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "l_orderkey: " << record.l_orderkey << ", l_linenumber: " << record.l_linenumber;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::l_orderkey, &key_base::l_linenumber> {};
 
     Integer l_partkey;
     Integer l_suppkey;
@@ -141,37 +168,35 @@ struct lineitem_t {
     Varchar<44> l_comment;
 };
 
-struct nation_t {
+struct lineitem_t : public lineitem_base, public RecordPrototype<lineitem_base, &lineitem_base::l_partkey, &lineitem_base::l_suppkey, &lineitem_base::l_quantity, &lineitem_base::l_extendedprice, &lineitem_base::l_discount, &lineitem_base::l_tax, &lineitem_base::l_returnflag, &lineitem_base::l_linestatus, &lineitem_base::l_shipdate, &lineitem_base::l_commitdate, &lineitem_base::l_receiptdate, &lineitem_base::l_shipinstruct, &lineitem_base::l_shipmode, &lineitem_base::l_comment> {};
+
+struct nation_base {
     static constexpr int id = 6;
-    struct Key {
+    struct key_base {
         static constexpr int id = 6;
         Integer n_nationkey;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "n_nationkey: " << record.n_nationkey;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::n_nationkey> {};
 
     Varchar<25> n_name;
     Integer n_regionkey;
     Varchar<152> n_comment;
 };
 
-struct region_t {
+struct nation_t : public nation_base, public RecordPrototype<nation_base, &nation_base::n_name, &nation_base::n_regionkey, &nation_base::n_comment> {};
+
+struct region_base {
     static constexpr int id = 7;
-    struct Key {
+    struct key_base {
         static constexpr int id = 7;
         Integer r_regionkey;
+    };
 
-        friend std::ostream& operator<<(std::ostream& os, const Key& record)
-        {
-            os << "r_regionkey: " << record.r_regionkey;
-            return os;
-        }
-    }
+    struct Key : public key_base, public KeyPrototype<key_base, &key_base::r_regionkey> {};
 
     Varchar<25> r_name;
     Varchar<152> r_comment;
 };
+
+struct region_t : public region_base, public RecordPrototype<region_base, &region_base::r_name, &region_base::r_comment> {};
