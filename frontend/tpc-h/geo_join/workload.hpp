81e735e82c9fe309d823bd811d514d8fce8225dd
alicia-lyu <wlyu27@wisc.edu>
2025-05-19 19:08:13 +0000

    Debugging

--- frontend/tpc-h/geo_join/workload.hpp
+++ frontend/tpc-h/geo_join/workload.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
 #include <chrono>
-#include <optional>
-#include <variant>
 
 #include "../../shared/Adapter.hpp"
 #include "../merge.hpp"
 #include "../tpch_workload.hpp"
-#include "Exceptions.hpp"
 #include "views.hpp"
 
 namespace geo_join
@@ -32,8 +29,6 @@ class GeoJoin
 
    Logger& logger;
 
-   long lookup_aborted = 0;
-
   public:
    GeoJoin(TPCH& workload, MergedTree& m, AdapterType<view_t>& v, AdapterType<states_t>& s, AdapterType<county_t>& c, AdapterType<city_t>& ci)
        : workload(workload),
@@ -47,49 +42,9 @@ class GeoJoin
    {
    }
 
-   ~GeoJoin()
-   {
-      if (lookup_aborted > 0)
-         std::cout << "\rGeoJoin: " << lookup_aborted << " point lookups aborted------------------------------------" << std::endl;
-   }
-
    // -------------------------------------------------------------
    // ---------------------- POINT LOOKUPS ------------------------
    // point lookups: one per view, one per all base tables
-
-   std::tuple<Integer, Integer, Integer, Integer> point_lookups_for_base()
-   {
-      auto result = std::make_tuple(0, 0, 0, 0);
-      try {
-         auto cik = point_query_by_base(false);
-         result = std::make_tuple(cik.nationkey, cik.statekey, cik.countykey, cik.citykey);
-      } catch (const std::exception& e) {
-         lookup_aborted++;  // ignore exceptions, possibly because of contentions
-      }
-      point_lookups_of_rest();
-      return result;
-   }
-
-   void point_lookups_for_view()
-   {
-      try {
-         auto cik = point_query_by_base(false);
-         point_query_by_view(cik.nationkey, cik.statekey, cik.countykey, cik.citykey, false);
-      } catch (const std::exception& e) {
-         lookup_aborted++;  // ignore exceptions
-      }
-      point_lookups_of_rest();
-   }
-
-   void point_lookups_for_merged()
-   {
-      try {
-         point_query_by_merged(false);
-      } catch (const std::exception& e) {
-         lookup_aborted++;  // ignore exceptions
-      }
-      point_lookups_of_rest();
-   }
 
    void point_lookups_of_rest()
    {
@@ -178,143 +133,60 @@ class GeoJoin
       point_query_by_view(nationkey, statekey, countykey, citykey);
    }
 
-   void point_query_by_view(Integer nationkey, Integer statekey, Integer countykey, Integer citykey, bool wait = true)
+   void point_query_by_view(Integer nationkey, Integer statekey, Integer countykey, Integer citykey)
    {
-      std::optional<view_t> vv = std::nullopt;
+      sort_key_t sk = sort_key_t{0, 0, 0, 0};
+      int produced = 0;
       view.scan(
           view_t::Key{nationkey, statekey, countykey, citykey},
-          [&](const view_t::Key&, const view_t& v) {
-             vv = v;
-             return false;
+          [&](const view_t::Key& vk, const view_t&) {
+             produced++;
+             if (sk == sort_key_t{0, 0, 0, 0}) {
+                sk = vk.jk;
+             } else if (sk != vk.jk) {
+                return false;
+             }
+             return true;
           },
           []() {});
-      if (wait) {
-         while (!vv.has_value()) {
-         }
-      } else {
-         if (vv.has_value()) {
-            throw std::runtime_error("View found");
-         }
-      }
+      std::cout << "It produced " << produced << " records.";  // following #2000 point-query-view performed.
    }
 
-   template <typename R>
-   static void seek_merged(std::shared_ptr<MergedScannerType<nation2_t, states_t, county_t, city_t>> scanner,
-                           const typename R::Key& k,
-                           std::optional<nation2_t>& nv,
-                           std::optional<states_t>& sv,
-                           std::optional<county_t>& cv,
-                           std::optional<city_t>& civ)
+   void point_query_by_merged()
    {
-      bool found = scanner->template seek<R>(k);
-      assert(found);
-      auto kv = scanner->current();
-      assert(kv != std::nullopt);
-      auto& [_, v] = *kv;
-      std::visit(overloaded{[&](const nation2_t& av) {
-                               if (std::is_same_v<R, nation2_t>)
-                                  nv = av;
-                               else
-                                  UNREACHABLE();
-                            },
-                            [&](const states_t& av) {
-                               if (std::is_same_v<R, states_t>)
-                                  sv = av;
-                               else
-                                  UNREACHABLE();
-                            },
-                            [&](const county_t& av) {
-                               if (std::is_same_v<R, county_t>)
-                                  cv = av;
-                               else
-                                  UNREACHABLE();
-                            },
-                            [&](const city_t& av) {
-                               if (std::is_same_v<R, city_t>)
-                                  civ = av;
-                               else
-                                  UNREACHABLE();
-                            }},
-                 v);
+      sort_key_t sk = sort_key_t{workload.getNationID(), getStateKey(), getCountyKey(), getCityKey()};
+
+      auto scanner = merged.getScanner();
+
+      scanner->template seekForPrev<nation2_t>(nation2_t::Key{sk.nationkey});
+
+      auto [_, produced] = scanner->template next_jk<sort_key_t, view_t>();
+
+      std::cout << "It produced " << produced << " records.";  // following #2000 point-query-merged performed.
    }
 
-   void point_query_by_merged(bool wait = true)
+   void point_query_by_base()
    {
-      std::shared_ptr<MergedScannerType<nation2_t, states_t, county_t, city_t>> scanner = std::move(merged.getScanner());
-      std::optional<nation2_t> nv = std::nullopt;
-      std::optional<states_t> sv = std::nullopt;
-      std::optional<county_t> cv = std::nullopt;
-      std::optional<city_t> civ = std::nullopt;
-      // find sort key and city
-      bool ret = scanner->template seekTyped<city_t>(city_t::Key{workload.getNationID(), getStateKey(), getCountyKey(), getCityKey()});
-      if (!ret)
-         return;
-      std::optional<sort_key_t> target_sk = std::nullopt;
-      auto target_kv = scanner->current();
-      assert(target_kv != std::nullopt);
-      auto& [k, v] = *target_kv;
-      std::visit(overloaded{[&](const nation2_t::Key&) { UNREACHABLE(); }, [&](const states_t::Key&) { UNREACHABLE(); },
-                            [&](const county_t::Key&) { UNREACHABLE(); }, [&](const city_t::Key& ak) { target_sk = ak.get_jk(); }},
-                 k);
-      std::visit(overloaded{[&](const nation2_t&) { UNREACHABLE(); }, [&](const states_t&) { UNREACHABLE(); },
-                            [&](const county_t&) { UNREACHABLE(); }, [&](const city_t& av) { civ = av; }},
-                 v);
-      if (wait) {
-         while (!target_sk.has_value()) {
-         }
-      } else {
-         if (!target_sk.has_value()) {
-            throw std::runtime_error("target_sk not found");
-         }
-      }
-      seek_merged<county_t>(scanner, county_t::Key{target_sk->nationkey, target_sk->statekey, target_sk->countykey}, nv, sv, cv, civ);
-      seek_merged<states_t>(scanner, states_t::Key{target_sk->nationkey, target_sk->statekey}, nv, sv, cv, civ);
-      seek_merged<nation2_t>(scanner, nation2_t::Key{target_sk->nationkey}, nv, sv, cv, civ);
+      sort_key_t sk = sort_key_t{workload.getNationID(), getStateKey(), getCountyKey(), getCityKey()};
 
-      view_t vv{nv.value(), sv.value(), cv.value(), civ.value()};
-   }
+      auto nation_scanner = nation.getScanner();
+      nation_scanner->seekForPrev(nation2_t::Key{sk.nationkey});
+      auto states_scanner = states.getScanner();
+      states_scanner->seekForPrev(states_t::Key{sk.nationkey, sk.statekey});
+      auto county_scanner = county.getScanner();
+      county_scanner->seekForPrev(county_t::Key{sk.nationkey, sk.statekey, sk.countykey});
+      auto city_scanner = city.getScanner();
+      city_scanner->seekForPrev(city_t::Key{sk.nationkey, sk.statekey, sk.countykey, sk.citykey});
 
-   city_t::Key point_query_by_base(bool wait = true)
-   {
-      std::optional<city_t::Key> cik = std::nullopt;
-      std::optional<city_t> civ = std::nullopt;
+      BinaryMergeJoin<sort_key_t, ns_t, nation2_t, states_t> binary_join_ns([&]() { return nation_scanner->next(); },
+                                                                            [&]() { return states_scanner->next(); });
+      BinaryMergeJoin<sort_key_t, nsc_t, ns_t, county_t> binary_join_nsc([&]() { return binary_join_ns.next(); },
+                                                                         [&]() { return county_scanner->next(); });
+      BinaryMergeJoin<sort_key_t, view_t, nsc_t, city_t> binary_join([&]() { return binary_join_nsc.next(); },
+                                                                     [&]() { return city_scanner->next(); });
+      int produced = binary_join.next_jk();
 
-      city.scan(
-          city_t::Key{workload.getNationID(), getStateKey(), getCountyKey(), getCityKey()},
-          [&](const city_t::Key& k, const city_t& v) {
-             cik = k;
-             civ = v;
-             return false;
-          },
-          []() {});
-      if (wait) {
-         while (!cik.has_value() || !civ.has_value()) {
-         }
-      } else {
-         if (!cik.has_value() || !civ.has_value()) {
-            throw std::runtime_error("City not found");
-         }
-      }
-
-      std::optional<county_t> cv = std::nullopt;
-      county.lookup1(county_t::Key{cik->nationkey, cik->statekey, cik->countykey}, [&](const county_t& v) { cv = v; });
-
-      std::optional<states_t> sv = std::nullopt;
-      states.lookup1(states_t::Key{cik->nationkey, cik->statekey}, [&](const states_t& v) { sv = v; });
-
-      std::optional<nation2_t> nv = std::nullopt;
-      nation.lookup1(nation2_t::Key{cik->nationkey}, [&](const nation2_t& v) { nv = v; });
-
-      if (wait) {
-         while (!nv.has_value() || !sv.has_value() || !cv.has_value() || !civ.has_value()) {
-         }
-      } else {
-         if (!nv.has_value() || !sv.has_value() || !cv.has_value() || !civ.has_value()) {
-            throw std::runtime_error("One of the keys not found");
-         }
-      }
-      view_t vv{nv.value(), sv.value(), cv.value(), civ.value()};
-      return cik.value();
+      std::cout << "It produced " << produced << " records.";  // following #2000 point-query-base performed.
    }
 
    // -------------------------------------------------------------
@@ -323,6 +195,10 @@ class GeoJoin
 
    void range_query_by_view()
    {
+      logger.reset();
+      std::cout << "GeoJoin::range_query_by_view()" << std::endl;
+      auto start = std::chrono::high_resolution_clock::now();
+
       auto nationkey = workload.getNationID();
       [[maybe_unused]] long produced = 0;
       view.scan(
@@ -334,99 +210,71 @@ class GeoJoin
              return true;
           },
           []() {});
+
+      std::cout << "\rRange querying materialized view for nation " << nationkey << " : " << (double)produced / 1000
+                << "k------------------------------------" << std::endl;
+      auto end = std::chrono::high_resolution_clock::now();
+      auto t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+      logger.log(t, ColumnName::ELAPSED, "range-query-view");
    }
 
    void range_query_by_merged()
    {
+      logger.reset();
+      std::cout << "GeoJoin::range_query_by_merged()" << std::endl;
+      auto start = std::chrono::high_resolution_clock::now();
+
       auto scanner = merged.getScanner();
       int n = workload.getNationID();
       scanner->template seekForPrev<nation2_t>(nation2_t::Key{n});
-      int curr_statekey, curr_countykey, curr_citykey;
-      std::optional<nation2_t> nv = std::nullopt;
-      std::optional<states_t> sv = std::nullopt;
-      std::optional<county_t> cv = std::nullopt;
-      std::optional<city_t> civ = std::nullopt;
-      bool end = false;
-      [[maybe_unused]] long produced = 0;
 
-      while (!end) {
-         auto kv = scanner->next();
-         if (kv == std::nullopt)
-            break;
-         auto& [k, v] = *kv;
-         std::visit(overloaded{[&](const nation2_t::Key& nk) {
-                                  if (nk.nationkey != n)
-                                     end = true;
-                               },
-                               [&](const states_t::Key& sk) {
-                                  assert(sk.nationkey == n);
-                                  cv = std::nullopt;
-                                  civ = std::nullopt;
-                                  curr_statekey = sk.statekey;
-                               },
-                               [&](const county_t::Key& ck) {
-                                  assert(ck.nationkey == n && ck.statekey == curr_statekey);
-                                  civ = std::nullopt;
-                                  curr_countykey = ck.countykey;
-                               },
-                               [&](const city_t::Key& ci) {
-                                  assert(ci.nationkey == n && ci.statekey == curr_statekey && ci.countykey == curr_countykey);
-                                  curr_citykey = ci.citykey;
-                               }},
-                    k);
-         std::visit(overloaded{[&](const nation2_t& av) { nv = av; }, [&](const states_t& av) { sv = av; }, [&](const county_t& av) { cv = av; },
-                               [&](const city_t& av) { civ = av; }},
-                    v);
-         if (nv.has_value() && sv.has_value() && cv.has_value() && civ.has_value()) {
-            view_t::Key vk{n, curr_statekey, curr_countykey, curr_citykey};
-            view_t vv{nv.value(), sv.value(), cv.value(), civ.value()};
-            TPCH::inspect_produced("Range querying merged: ", produced);
-         }
+      sort_key_t curr_jk = sort_key_t{n, 0, 0, 0};
+      long produced = 0;
+      long curr_produced = 0;
+      while (curr_jk.nationkey == n) {
+         std::tie(curr_jk, curr_produced) = scanner->template next_jk<sort_key_t, view_t>();
+         produced += curr_produced;
       }
-   }
+      std::cout << "\rRange querying merged for nation " << n << " : " << produced << " records------------------------------------" << std::endl;
 
-   template <typename R>
-   static std::optional<std::pair<typename R::Key, R>> conditional_next(std::shared_ptr<ScannerType<R>> scanner, int n)
-   {
-      auto kv = scanner->next();
-      if (kv == std::nullopt)
-         return std::nullopt;
-      auto& [k, v] = *kv;
-      if (k.nationkey != n)
-         return std::nullopt;
-      return kv;
-   }
-
-   template <typename JK, typename JR, typename... Rs>
-   static std::optional<std::pair<typename JR::Key, JR>> conditional_next(BinaryMergeJoin<JK, JR, Rs...>& join, int n)
-   {
-      auto kv = join.next();
-      if (kv == std::nullopt)
-         return std::nullopt;
-      auto& [k, v] = *kv;
-      if (k.jk.nationkey != n)
-         return std::nullopt;
-      return kv;
+      auto end = std::chrono::high_resolution_clock::now();
+      auto t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+      logger.log(t, ColumnName::ELAPSED, "range-query-merged");
    }
 
    void range_query_by_base()
    {
+      logger.reset();
+      std::cout << "GeoJoin::range_query_by_base()" << std::endl;
+      auto start = std::chrono::high_resolution_clock::now();
+
       auto n = workload.getNationID();
 
-      std::shared_ptr<ScannerType<nation2_t>> nation_scanner = std::move(nation.getScanner());
-      std::shared_ptr<ScannerType<states_t>> states_scanner = std::move(states.getScanner());
-      std::shared_ptr<ScannerType<county_t>> county_scanner = std::move(county.getScanner());
-      std::shared_ptr<ScannerType<city_t>> city_scanner = std::move(city.getScanner());
+      auto nation_scanner = nation.getScanner();
+      nation_scanner->seekForPrev(nation2_t::Key{n});
+      auto states_scanner = states.getScanner();
+      states_scanner->seek(states_t::Key{n, 0});
+      auto county_scanner = county.getScanner();
+      county_scanner->seek(county_t::Key{n, 0, 0});
+      auto city_scanner = city.getScanner();
+      city_scanner->seek(city_t::Key{n, 0, 0, 0});
 
-      BinaryMergeJoin<sort_key_t, ns_t, nation2_t, states_t> binary_join_ns([&]() { return conditional_next<nation2_t>(nation_scanner, n); },
-                                                                            [&]() { return conditional_next<states_t>(states_scanner, n); });
-      BinaryMergeJoin<sort_key_t, nsc_t, ns_t, county_t> binary_join_nsc(
-          [&]() { return conditional_next<sort_key_t, ns_t, nation2_t, states_t>(binary_join_ns, n); },
-          [&]() { return conditional_next<county_t>(county_scanner, n); });
-      BinaryMergeJoin<sort_key_t, view_t, nsc_t, city_t> binary_join(
-          [&]() { return conditional_next<sort_key_t, nsc_t, ns_t, county_t>(binary_join_nsc, n); },
-          [&]() { return conditional_next<city_t>(city_scanner, n); });
-      binary_join.run();
+      BinaryMergeJoin<sort_key_t, ns_t, nation2_t, states_t> binary_join_ns([&]() { return nation_scanner->next(); },
+                                                                            [&]() { return states_scanner->next(); });
+      BinaryMergeJoin<sort_key_t, nsc_t, ns_t, county_t> binary_join_nsc([&]() { return binary_join_ns.next(); },
+                                                                         [&]() { return county_scanner->next(); });
+      BinaryMergeJoin<sort_key_t, view_t, nsc_t, city_t> binary_join([&]() { return binary_join_nsc.next(); },
+                                                                     [&]() { return city_scanner->next(); });
+      int produced = 0;
+      while (binary_join.current_jk.nationkey == n || binary_join.current_jk == sort_key_t::max()) {
+         produced += binary_join.next_jk();
+      }
+
+      std::cout << "\rRange querying base for nation " << n << " : " << produced << " records------------------------------------"
+                << std::endl;
+      auto end = std::chrono::high_resolution_clock::now();
+      auto t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+      logger.log(t, ColumnName::ELAPSED, "range-query-base");
    }
 
    // -------------------------------------------------------------
